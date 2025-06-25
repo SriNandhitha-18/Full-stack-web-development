8cef04d97fa33bc7e77e85d4376eb0aba1b5c5c6
SriNandhitha-18 <srinandhitha20@gmail.com>
2025-06-25 11:24:01 +0530

    Add files via upload

---
+++ Javascript/triviabot.js
@@ -0,0 +1,19 @@
+** start of script.js **
+
+console.log("Hello! I'm your coding fun fact guide!");
+let botName="MeowBot";
+let botLocation="Earth";
+let favoriteLanguage="Python";
+console.log("My name is "+ botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+let codingFact = "bot likes coding in " + favoriteLanguage;
+console.log(codingFact);
+codingFact =  "I like" + favoriteLanguage;
+console.log(codingFact);
+codingFact = "bot likes" + favoriteLanguage;
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation+ ".");
+
+
+** end of script.js **
+
