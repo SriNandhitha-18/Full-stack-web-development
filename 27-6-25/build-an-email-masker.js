f4d8ffa04f93952e5730f28c223aae545fc29176
SriNandhitha-18 <srinandhitha20@gmail.com>
2025-06-27 19:00:23 +0530

    Add files via upload

---
+++ 27-6-25/build-an-email-masker.js
@@ -0,0 +1,29 @@
+** start of script.js **
+
+function maskEmail(email) {
+
+  let pos = email.indexOf("@");
+  let maskedLength = pos;
+
+  let begin = email.slice(0, 1);
+  let end = email.slice(pos - 1);
+
+  let replace = "*";
+  let replaced = replace.repeat(maskedLength - 2);
+
+  let finishedEmail = begin + replaced + end;
+
+  return finishedEmail;
+
+};
+
+let email = "apple.pie@example.com";
+
+console.log(maskEmail(email));
+
+email = "freecodecamp@example.com";
+
+console.log(maskEmail(email));
+
+** end of script.js **
+
