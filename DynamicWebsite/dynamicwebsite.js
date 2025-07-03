8e6f8e12af4b0347755053e92da142029ddd901e
SriNandhitha-18 <srinandhitha20@gmail.com>
2025-07-03 21:57:33 +0530

    Add files via upload

---
+++ DynamicWebsite/dynamicwebsite.js
@@ -0,0 +1,18 @@
+document.getElementById("link").addEventListener("click", function() {
+    alert("You are redirected to the page");
+});
+ let block1 = document.getElementById("block1");
+block1.addEventListener('mouseenter', function() {
+    block1.style.backgroundColor = 'chocolate';
+  });
+  block1.addEventListener("mouseleave", function() {
+    block1.style.backgroundColor = 'goldenrod';
+  });
+let browny = document.getElementById("browny");
+browny.addEventListener('mouseenter', function() {
+    browny.style.backgroundColor = 'chocolate';
+  });
+  browny.addEventListener("mouseleave", function() {
+    browny.style.backgroundColor = 'goldenrod';
+  });
+
